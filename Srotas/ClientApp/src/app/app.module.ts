d8b49ef41f906b00efe1746ad8325e33d59c65bb
tehFruit <76218849+tehFruit@users.noreply.github.com>
2022-04-07 12:52:18 +0300

    Works

--- Srotas/ClientApp/src/app/app.module.ts
+++ Srotas/ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -7,32 +7,21 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { WheelsTableComponent } from './components/wheels-table/wheels-table.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {MatTableModule} from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    WheelsTableComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    MatTableModule,
-    MatIconModule,
-    MatButtonModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'wheels', component: WheelsTableComponent, pathMatch: 'full' }
-    ]),
-    BrowserAnimationsModule
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Srotas/ClientApp/src/app/models/Wheels.ts
+++
@@ -1,9 +0,0 @@
-export class Wheels {
-    id: number;
-    kaina: number;
-    pavadinimas: string;
-    parduotas: boolean;
-    dydis: string;
-    plotis: number;
-    gamintojoas: string;
-}
