82113644d2dfa2094426626cda7e5c86b8da784e
tehFruit <76218849+tehFruit@users.noreply.github.com>
2022-05-26 15:44:24 +0300

    Users API

---
+++ Srotas/ClientApp/src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Buyer } from './../models/Buyer';
+import { Seller } from './../models/Seller';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  readonly userType = 'Pirkejas';
+  readonly baseUrl = 'https://localhost:7201/api/User';
+  constructor(private http: HttpClient) { }
+
+  public getUserType(){
+    return this.userType;
+  }
+
+  public getSeller() : Observable<Seller> {
+    return this.http.get<Seller>(this.baseUrl + '/GetSeller');
+  }
+
+  public getBuyer() : Observable<Buyer>{
+    return this.http.get<Buyer>(this.baseUrl + '/GetBuyer');
+  }
+
+  public UpdateSellerRating(rating: number) : Observable<Seller>{
+    return this.http.put<Seller>(this.baseUrl + '/UpdateSeller', rating);
+  }
+
+  public UpdateBuyerBalance(balance: number) : Observable<Buyer>{
+    return this.http.put<Buyer>(this.baseUrl + '/UpdateBuyer', balance);
+  }
+}
---
+++ Srotas/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Srotas.Data;
+using Srotas.Models;
+
+namespace Srotas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly AppDbContext dbContext;
+
+        public UserController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        [Route("GetBuyer")]
+        public async Task<IActionResult> GetBuyer()
+        {
+            var buyer = dbContext.Pirkejas.FirstOrDefault();
+            return Ok(buyer);
+        }
+
+        [HttpGet]
+        [Route("GetSeller")]
+        public async Task<IActionResult> GetSeller()
+        {
+            var seller = dbContext.Pardavejas.FirstOrDefault();
+            return Ok(seller);
+        }
+
+        [HttpPut]
+        [Route("UpdateBuyer")]
+        public async Task<IActionResult> UpdateBuyer([FromBody] int balance)
+        {
+            var buyer = dbContext.Pirkejas.FirstOrDefault();
+            buyer.Balansas = balance;
+            await dbContext.SaveChangesAsync();
+            return Ok(buyer);
+        }
+
+        [HttpPut]
+        [Route("UpdateSeller")]
+        public async Task<IActionResult> UpdateSeller([FromBody] int rating)
+        {
+            var seller = dbContext.Pardavejas.FirstOrDefault();
+            seller.Reitingas = rating;
+            await dbContext.SaveChangesAsync();
+            return Ok(seller);
+        }
+    }
+}
